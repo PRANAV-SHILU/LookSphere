215189ee3966cfe73599a62261441a36ef05a1bb
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-06-08 13:44:48 +0530

    mongoose connection for mongodb

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dns from "dns";
 
+// Fix: Node.js's async DNS resolver fails on SRV lookups on some networks, Force it to use Google's public DNS which correctly handles mongodb+srv:// SRV records.
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 
@@ -11,18 +15,28 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 
 // 404 handler - for unmatched routes
 app.use((err, req, res, next) => {
   res.status(404).send("path not found!");
 });
 
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("error while connecting mongoose : ", err);
+  });
